700d1d414c6f4c233ebdc7b52a0b3bea6089c079
Unknown <maszeq@gmail.com>
2017-04-18 11:06:30 +0200

    Bug report
    VideoPlayer feature introduced in Unity 5.6 works properly in Unity, but after being deployed to Hololens Device no Video is shown and no Sound can be heard.

---
+++ BugReportVideoPlayerOnHololens/Assets/TapTutorial.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TapTutorial : MonoBehaviour
+{
+
+    private VideoPlayer videoPlayer;
+    private AudioSource audioSource;
+    public VideoClip videoClip;
+
+    void Start()
+    {
+        GameObject camera = GameObject.Find("HoloLensCamera");
+        videoPlayer = camera.AddComponent<VideoPlayer>();
+        audioSource = camera.AddComponent<AudioSource>();
+
+        //Disable Play on Awake for both Video and Audio
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+        videoPlayer.clip = videoClip;
+
+        // Video settings
+        videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
+        videoPlayer.aspectRatio = VideoAspectRatio.FitVertically;
+        videoPlayer.targetCameraAlpha = 1.0F;
+        videoPlayer.isLooping = false;
+
+        // Audio settings
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        videoPlayer.loopPointReached += EndReached;
+
+        audioSource.Play();
+        videoPlayer.Play();
+
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        Debug.Log("End reached!");
+        StartCoroutine(fadeOutVideo());
+    }
+
+    IEnumerator fadeOutVideo()
+    {
+        while (videoPlayer.targetCameraAlpha > 0.0f)
+        {
+            videoPlayer.targetCameraAlpha -= 0.01f;
+            yield return null;
+        }
+    }
+
+    IEnumerator fadeInVideo()
+    {
+        while (videoPlayer.targetCameraAlpha <= 0.0f)
+        {
+            videoPlayer.targetCameraAlpha += 0.01f;
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Frame " + videoPlayer.frame);
+    }
+}
